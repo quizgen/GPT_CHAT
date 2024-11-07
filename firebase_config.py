5cce62c868257774afa93ee983a60b5b81b324bd
kim jae young <162528651+kimjy0411@users.noreply.github.com>
2024-11-08 01:28:34 +0900

    Update firebase_config.py

--- firebase_config.py
+++ firebase_config.py
@@ -1,13 +1,13 @@
 import firebase_admin
 from firebase_admin import credentials, firestore, storage
 
-# Firebase 초기화 확인 및 초기화
-if not firebase_admin._apps:
-    # Firebase 서비스 계정 키 경로
-    cred = credentials.Certificate("gpt-chating-c72cacf446d4.json")
-    firebase_admin.initialize_app(cred, {
-        'storageBucket': 'gpt-chating.appspot.com'  # Firebase Storage 버킷 이름 설정
-    })
+# Firebase 서비스 계정 키 경로
+cred = credentials.Certificate("gpt-chating-c72cacf446d4.json")
+
+# Firebase 초기화 (Storage 버킷 이름 포함)
+firebase_admin.initialize_app(cred, {
+    'storageBucket': 'gpt-chating.appspot.com'  # Firebase Storage 버킷 이름 설정
+})
 
 # Firestore 클라이언트 초기화
 db = firestore.client()
